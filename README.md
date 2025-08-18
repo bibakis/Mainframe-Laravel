a06640f31275f364f40963b2ea562bb4f330ebd0
Bibakis Vangelis <bibakisv@gmail.com>
2025-08-18 03:24:50 +0300

    Asset loading helper description

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-## About Mainframe Laravel
+### Alani util pack for Laravel
 
-This is the place where I keep code that I want to share across my web apps.
+Created and distributed for free by [Bibakis](https://bibakis.com/) under the MIT License.  
+
+---
+
+#### Asset loading helper
+
+Force the browser to always fetch the latest css/js while still caching files if there are no changes. You may have heard this as "cache busting".
+
+Place your css & js files in the /public directory and then simply do the following from any blade file:  
+@@asset('web/css/styles.css')  
+@@asset('web/js/basic.js')
+
+This will load the files with the appropriate html tag and auto append a version identifier based on the time the file was last updated. For example:  
+https://example.com/web/css/styles.css**?v=1755474797**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,26 @@
     </head>
 <body>
     <div class="container">
-        <a href="">Alani "framework"</a>
+        <h3>Alani util pack for Laravel</h3>
+        Created and distributed for free by <a href="https://bibakis.com/" target="_blank">Bibakis</a> under the MIT License.<br>
+        <a href="https://github.com/bibakis/Alani" target="_blank">Github project page</a>
 
+        <br><br>
+        <hr>
 
+        <h4>Asset loading helper</h4>
+        <p>
+            Force the browser to always fetch the latest css/js while still caching files if there are no changes. You may have heard this as "cache busting".
+        </p>
+        <p>
+            Place your css & js files in the /public directory and then simply do the following from any blade file:<br><br>
+            @@asset('web/css/styles.css')<br>
+            @@asset('web/js/basic.js')
+        </p>
+        <p>
+            This will load the files with the appropriate html tag and auto append a version identifier based on the time the file was last updated.
+            For example: <br>https://example.com/web/css/styles.css<b>?v=1755474797</b>
+        </p>
     </div>
 </body>
 </html>
